368a65488bc7046d7276e7699f7bd54a38b1505b
1995hnagamin <1995.hnagamin@gmail.com>
2014-05-11 18:50:30 +0900

    Define numerical multipliers
    new procedures:
    multiplying-affix, multiplying-infix
    mono, di, tri, tetra, penta, hexa, hepta, octa, nona
    conta, cta, lia

---
+++ organic.scm
@@ -0,0 +1,25 @@
+(define (multiplying-affix n)
+  (lambda args
+    (cond
+      ((null? args) n)
+      ((procedure? (car args)) ((car args) n))
+      (else (apply + (cons n args))))))
+
+(define (multiplying-infix k)
+  (lambda (n)
+    (* k n)))
+
+(define mono  (multiplying-affix 1))
+(define di    (multiplying-affix 2))
+(define tri   (multiplying-affix 3))
+(define tetra (multiplying-affix 4))
+(define penta (multiplying-affix 5))
+(define hexa  (multiplying-affix 6))
+(define hepta (multiplying-affix 7))
+(define octa  (multiplying-affix 8))
+(define nona  (multiplying-affix 9))
+
+(define conta (multiplying-infix 10))
+(define cta   (multiplying-infix 100))
+(define lia   (multiplying-infix 1000))
+
